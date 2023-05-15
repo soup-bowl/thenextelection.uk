7b6d5dc39e28e5ae41d417ec88bc1553efd78412
soup-bowl <code@soupbowl.io>
2023-05-15 17:06:55 +0000

    Added a "how to vote" link.

--- src/Page/App.tsx
+++ src/Page/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Countdown from "../Components/Countdown";
 import Main from "../Components/MainStyle";
 import { calculateCountdown, ITimeCalculation } from "../Functions/TimeCalculation";
+import styled from "@emotion/styled";
 
 const App = () => {
 	const [countdownTime, setCountdownTime] = useState<ITimeCalculation | undefined>(undefined);
@@ -15,12 +16,28 @@ const App = () => {
 		}, 1000);
 	}, []);
 
+	const Info = styled.p({
+		margin: 0,
+		fontSize: '0.75em',
+		textShadow: '0px 0px 10px black'
+	});
+
+	const Link = styled.a({
+		color: 'white',
+		margin: 0,
+		fontSize: '0.75em',
+		textShadow: '0px 0px 10px black',
+		textDecoration: 'none',
+		':hover': {
+			textDecoration: 'underline'
+		}
+	});
+
 	return (
 		<Main>
-			<p style={{ margin: 0, fontSize: '0.75em', textShadow: '0px 0px 10px black' }}>
-				The next UK General Election is
-			</p>
+			<Info>The next UK General Election is</Info>
 			{countdownTime !== undefined && <Countdown counter={countdownTime} />}
+			<Link style={{ marginTop: 2 }} href="https://www.gov.uk/how-to-vote" target="_blank">How to vote?</Link>
 		</Main>
 	);
 }
