63f5c86dcde05e5fe4751a396c9e0b3cdb876ce7
soup-bowl <code@soupbowl.io>
2023-09-01 21:48:30 +0100

    Shifted data.

--- src/Page/App.tsx
+++ src/Page/App.tsx
@@ -3,8 +3,8 @@ import Countdown from "../Components/Countdown";
 import Main from "../Components/MainStyle";
 import { calculateCountdown, ITimeCalculation } from "../Functions/TimeCalculation";
 import styled from "@emotion/styled";
-import { Modal } from "../Components/Modal";
 import useInterval from "../Functions/useInterval";
+import { InfoModal } from "./Modal";
 
 const App = () => {
 	const [dialog, setDialogState] = useState<boolean>(false);
@@ -34,38 +34,12 @@ const App = () => {
 		}
 	});
 
-	const Footnote = styled.p({
-		color: '#818181',
-		textAlign: 'center',
-	});
-
 	return (
 		<Main>
 			<Info>The next UK General Election is</Info>
 			<Countdown counter={countdownTime} />
 			<Link style={{ marginTop: 2 }} onClick={() => setDialogState(true)}>More info</Link>
-			<Modal open={dialog} onClose={() => setDialogState(false)}>
-				<h2>About the Site</h2>
-				<p>
-					<strong>The Next Election UK</strong> just provides a simple, quick countdown to the next UK General
-					Election. The website was made by <strong><a href="https://github.com/soup-bowl">soup-bowl
-					</a></strong> and the code is <strong><a href="https://github.com/soup-bowl/thenextelection.uk">Open Source</a></strong>.
-				</p>
-				<h2>What is the source of the next election date?</h2>
-				<p>
-					The <strong>General Election countdown</strong> information is a rough figure based on information
-					from the <a href="https://www.instituteforgovernment.org.uk/article/explainer/calling-general-election">
-						Institute for Government
-					</a> guidance on how a general election is called. If the ruling party calls for an early general
-					election, this figure will be updated to reflect that.
-				</p>
-				<h2>How do I Vote?</h2>
-				<p>
-					You can find information about your local council votes and how to register to vote on the UK
-					Government <a href="https://www.gov.uk/how-to-vote">Register to Vote</a> page.
-				</p>
-				<Footnote>This website is not affiliated with the UK Government or any Political Party.</Footnote>
-			</Modal>
+			<InfoModal open={dialog} onClose={() => setDialogState(false)} />
 		</Main>
 	);
 }
---
+++ src/Page/Modal.tsx
@@ -0,0 +1,59 @@
+import styled from "@emotion/styled";
+import { Modal } from "../Components/Modal";
+
+interface Props {
+	open: boolean;
+	onClose: () => void;
+}
+
+const Footnote = styled.p({
+	color: '#818181',
+	textAlign: 'center',
+});
+
+export const InfoModal = ({ open, onClose }: Props) => {
+	const date = new Date(import.meta.env.VITE_ELECTION_DATE ?? '');
+	const reason = Number(import.meta.env.VITE_ELECTION_REASON ?? '0');
+
+	const electionReason = () => {
+		switch(reason) {
+			default:
+			case 0:
+				return "unknown";
+			case 1:
+				return "the end of the Political term";
+			case 2:
+				return "the incumbent Prime Minister called a General Election";
+		}
+	}
+
+	return (
+		<Modal open={open} onClose={onClose}>
+			<h2>About the Site</h2>
+			<p>
+				<strong>The Next Election UK</strong> just provides a simple, quick countdown to the next UK General
+				Election. The website was made by <strong><a href="https://github.com/soup-bowl">soup-bowl
+				</a></strong> and the code is <strong><a href="https://github.com/soup-bowl/thenextelection.uk">Open Source</a></strong>.
+			</p>
+			<h2>When exactly is the next election?</h2>
+			<p>
+				The current estimates place the next election at <strong>{date.toLocaleString('default', { month: 'long' })} {date.getFullYear()}</strong>.
+				The reason for this is <strong>{electionReason()}</strong>.
+			</p>
+			<h2>What is the source of the next election date?</h2>
+			<p>
+				The <strong>General Election countdown</strong> information is a rough figure based on information
+				from the <a href="https://www.instituteforgovernment.org.uk/article/explainer/calling-general-election">
+					Institute for Government
+				</a> guidance on how a general election is called. If the ruling party calls for an early general
+				election, this figure will be updated to reflect that.
+			</p>
+			<h2>How do I Vote?</h2>
+			<p>
+				You can find information about your local council votes and how to register to vote on the UK
+				Government <a href="https://www.gov.uk/how-to-vote">Register to Vote</a> page.
+			</p>
+			<Footnote>This website is not affiliated with the UK Government or any Political Party.</Footnote>
+		</Modal>
+	);
+}
