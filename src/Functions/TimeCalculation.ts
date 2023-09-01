fee9f6a0a68228c1055abec7d42e7a6c775e0eca
soup-bowl <code@soupbowl.io>
2023-09-01 21:57:15 +0100

    GPT calc improvement.

--- src/Functions/TimeCalculation.ts
+++ src/Functions/TimeCalculation.ts
@@ -7,24 +7,34 @@ export interface ITimeCalculation {
 	seconds: number;
 }
 
-// Function written by ChatGPT.
 export const calculateCountdown = (targetDate : Date) : ITimeCalculation => {
 	const now = new Date();
 
-	// calculate differences between target date and now
+	// calculate differences in years, months, and days
+	let years = targetDate.getFullYear() - now.getFullYear();
+	let months = targetDate.getMonth() - now.getMonth();
+	let days = targetDate.getDate() - now.getDate();
+
+	if (days < 0) {
+		months--;
+		const lastFullMonth = new Date(targetDate.getFullYear(), targetDate.getMonth() - 1, 1);
+		days += new Date(lastFullMonth.getFullYear(), lastFullMonth.getMonth() + 1, 0).getDate();
+	}
+
+	if (months < 0) {
+		years--;
+		months += 12;
+	}
+
+	// calculate differences in hours, minutes, and seconds
 	const differenceInSeconds = Math.floor((targetDate.getTime() - now.getTime()) / 1000);
 	const differenceInMinutes = Math.floor(differenceInSeconds / 60);
 	const differenceInHours = Math.floor(differenceInMinutes / 60);
-	const differenceInDays = Math.floor(differenceInHours / 24);
-	let differenceInMonths = targetDate.getMonth() - now.getMonth() + 12 * (targetDate.getFullYear() - now.getFullYear());
-	const differenceInYears = differenceInMonths >= 12 ? Math.floor(differenceInMonths / 12) : 0;
-	differenceInMonths = differenceInMonths % 12;
 
-	// calculate remaining time in years, months, days, hours, minutes, and seconds
 	const remainingTime = {
-		years: differenceInYears,
-		months: differenceInMonths,
-		days: differenceInDays % 30,
+		years: years,
+		months: months,
+		days: days,
 		hours: differenceInHours % 24,
 		minutes: differenceInMinutes % 60,
 		seconds: differenceInSeconds % 60,
