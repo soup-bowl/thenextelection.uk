b2712e2ab3e1fc13b6b26e4bbf99cc86dfcd3ff1
soup-bowl <code@soupbowl.io>
2024-01-25 21:29:46 +0000

    Show a message during election time

--- src/Page/App.tsx
+++ src/Page/App.tsx
@@ -8,9 +8,10 @@ import { InfoModal } from "./Modal";
 const App = () => {
 	const [dialog, setDialogState] = useState<boolean>(false);
 	const [countdownTime, setCountdownTime] = useState<ITimeCalculation | undefined>(undefined);
+	const isElectionTime: boolean = import.meta.env.VITE_ELECTION === '1';
 
 	useInterval(() => {
-		if (!dialog) {
+		if (!dialog && !isElectionTime) {
 			setCountdownTime(
 				calculateCountdown(new Date(import.meta.env.VITE_ELECTION_DATE ?? ''))
 			);
@@ -19,12 +20,21 @@ const App = () => {
 
 	return (
 		<Main>
-			<p style={{
-				margin: 0,
-				fontSize: '0.75em',
-				textShadow: '0px 0px 10px black'
-			}}>The next UK General Election is</p>
-			<Countdown counter={countdownTime} />
+			{!isElectionTime ?
+				<>
+					<p style={{
+						margin: 0,
+						fontSize: '0.75em',
+						textShadow: '0px 0px 10px black'
+					}}>The next UK General Election is</p>
+					<Countdown counter={countdownTime} />
+				</>
+				:
+				<p style={{
+					margin: 10,
+					fontSize: '1.5em',
+					textShadow: '0px 0px 10px black'
+				}}>An election is ongoing</p>}
 			<a className="main-link" onClick={() => setDialogState(true)}>More info</a>
 			<InfoModal open={dialog} onClose={() => setDialogState(false)} />
 		</Main>
