694187cdcff638d80b043e7df3bf1ef524fa7271
soup-bowl <code@soupbowl.io>
2024-02-10 22:25:40 +0000

    Stop the brief election time sign.

--- src/Page/App.tsx
+++ src/Page/App.tsx
@@ -7,7 +7,7 @@ import { InfoModal } from "./Modal";
 import { IColour, IElectionData } from "../interface";
 import yaml from 'yaml';
 
-const hslToString = ( colour: IColour ) => {
+const hslToString = (colour: IColour) => {
 	colour.l = colour.l - (colour.l / 2) - 5;
 	return `hsl(${colour.h},${colour.s}%,${colour.l}%)`;
 };
@@ -20,13 +20,13 @@ const App = () => {
 
 	useEffect(() => {
 		fetch(`https://raw.githubusercontent.com/soup-bowl/thenextelection.uk/main/election.yml`)
-		.then(response => response.text())
-		.then(yamlText => yaml.parse(yamlText))
-		.then((data:IElectionData) => {
-			console.log("Data", data);
-			setElectionData(data);
-			setColour(hslToString(data.config.parties.find(p => p.abbr === data.data.party)!.color));
-		})
+			.then(response => response.text())
+			.then(yamlText => yaml.parse(yamlText))
+			.then((data: IElectionData) => {
+				console.log("Data", data);
+				setElectionData(data);
+				setColour(hslToString(data.config.parties.find(p => p.abbr === data.data.party)!.color));
+			})
 	}, []);
 
 	useInterval(() => {
@@ -39,24 +39,31 @@ const App = () => {
 
 	return (
 		<Main Colour={colour}>
-			{(electionData !== undefined && !electionData.data.election) ?
+			{electionData !== undefined ?
 				<>
-					<p style={{
-						margin: 0,
-						fontSize: '0.75em',
-						textShadow: '0px 0px 10px black'
-					}}>The next UK General Election is</p>
-					<Countdown counter={countdownTime} />
-				</>
-				:
-				<p style={{
+					{!electionData.data.election ?
+						<>
+							<p style={{
+								margin: 0,
+								fontSize: '0.75em',
+								textShadow: '0px 0px 10px black'
+							}}>The next UK General Election is</p>
+							<Countdown counter={countdownTime} />
+						</>
+						:
+						<p style={{
+							margin: 10,
+							fontSize: '1.5em',
+							textShadow: '0px 0px 10px black'
+						}}>An election is ongoing</p>}
+				</> : <p style={{
 					margin: 10,
-					fontSize: '1.5em',
+					fontSize: '1em',
 					textShadow: '0px 0px 10px black'
-				}}>An election is ongoing</p>}
+				}}>Loading...</p>}
 			<a className="main-link" onClick={() => setDialogState(true)}>More info</a>
 			<InfoModal
-				ElectionDate={(electionData!== undefined) ? new Date(electionData.data.date) : undefined}
+				ElectionDate={(electionData !== undefined) ? new Date(electionData.data.date) : undefined}
 				Reason={electionData?.config.reasons.find(p => p.key === electionData.data.reason)?.message}
 				IsElection={electionData?.data.election}
 				open={dialog}
