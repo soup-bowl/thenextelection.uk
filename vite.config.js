01447c111019119e40c9bee49679c44453f1ff50
soup-bowl <code@soupbowl.io>
2024-05-24 22:54:54 +0000

    Disabled pwa functionality for now

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,10 @@
 import { defineConfig } from "vite"
-import { VitePWA } from "vite-plugin-pwa"
 import preact from "@preact/preset-vite"
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		preact(),
-		VitePWA({
-			registerType: "autoUpdate",
-			includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
-			manifest: {
-				short_name: "Next Election",
-				name: "The Next Election UK",
-				description:
-					"Awaiting the next government, or to watch the current grow stronger? Keep an eye on the countdown",
-				icons: [
-					{
-						src: "icon.png",
-						type: "image/png",
-						sizes: "144x144",
-						purpose: "any",
-					},
-				],
-				start_url: ".",
-				display: "standalone",
-				theme_color: "#070037",
-				background_color: "#070037",
-			},
-		}),
-	],
+	plugins: [preact()],
 	server: {
 		host: true,
 		port: 3000,
